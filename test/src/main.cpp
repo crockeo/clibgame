ab6124429b4cb224aef389afcab0bcfc9a93bce1
Cerek Hillen <crockeo@gmail.com>
2015-02-08 12:48:06 -0500

    Started working on a testing suite of sorts with the library.

---
+++ test/src/main.cpp
@@ -0,0 +1,13 @@
+//////////////
+// Includes //
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <clibgame.hpp>
+
+//////////
+// Code //
+
+// The entry point.
+int main(int argc, char** argv) {
+    std::cout << "Hello world" << std::endl;
+}
