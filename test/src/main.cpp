0a1526a32be86c50791304eb62320bc21a17aefd
Cerek Hillen <crockeo@gmail.com>
2015-02-15 22:28:37 -0500

    Started to work on more testing stuff.

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -11,68 +11,78 @@
 //////////
 // Code //
 
+#define NEW false
 #define UPDATES_PER_SECOND 120
 
 // The entry point.
 int main(int argc, char** argv) {
-    if (!glfwInit()) {
-        std::cerr << "Could not start GLFW." << std::endl;
-        return 1;
-    }
+    if (NEW) {
+        clibgame::EngineConfig cfg;
+        clibgame::ECP defaultECP;
 
-    // Creating the window.
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        clibgame::startEngine(cfg, defaultECP, "res/assetspec.txt");
 
-    GLFWwindow* window = glfwCreateWindow(
-        640, 480,
-        "clibgame test",
-        nullptr,
-        nullptr
-    );
+        return 0;
+    } else {
+        if (!glfwInit()) {
+            std::cerr << "Could not start GLFW." << std::endl;
+            return 1;
+        }
 
-    if (window == nullptr) {
-        std::cerr << "Could not create window." << std::endl;
+        // Creating the window.
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        GLFWwindow* window = glfwCreateWindow(
+            640, 480,
+            "clibgame test",
+            nullptr,
+            nullptr
+        );
+
+        if (window == nullptr) {
+            std::cerr << "Could not create window." << std::endl;
+            glfwTerminate();
+            return 1;
+        }
+
+        // Setting some OpenGL stuff.
+        glfwMakeContextCurrent(window);
+        glewExperimental = GL_TRUE;
+        if (glewInit() != GLEW_OK) {
+            std::cout << "Could not initialize GLEW." << std::endl;
+            glfwTerminate();
+            return 1;
+        }
+
+        glClearColor(1.f, 0.f, 1.f, 1.f);
+        glEnable(GL_BLEND);
+        glEnable(GL_DEPTH);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        // Preparing rendering.hpp
+        prepareRendering();
+
+        // Starting the game loop thingy?
+        clibgame::Delta d;
+        while (!glfwWindowShouldClose(window)) {
+            float t = d.since();
+            if (t < 1.f / UPDATES_PER_SECOND)
+                clibgame::delayThread(1.f / UPDATES_PER_SECOND - t);
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            doRendering();
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        // Closing up shop.
         glfwTerminate();
-        return 1;
+        cleanRendering();
+        return 0;
     }
-
-    // Setting some OpenGL stuff.
-    glfwMakeContextCurrent(window);
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cout << "Could not initialize GLEW." << std::endl;
-        glfwTerminate();
-        return 1;
-    }
-
-    glClearColor(1.f, 0.f, 1.f, 1.f);
-    glEnable(GL_BLEND);
-    glEnable(GL_DEPTH);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    // Preparing rendering.hpp
-    prepareRendering();
-
-    // Starting the game loop thingy?
-    clibgame::Delta d;
-    while (!glfwWindowShouldClose(window)) {
-        float t = d.since();
-        if (t < 1.f / UPDATES_PER_SECOND)
-            clibgame::delayThread(1.f / UPDATES_PER_SECOND - t);
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        doRendering();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // Closing up shop.
-    glfwTerminate();
-    cleanRendering();
-    return 0;
 }
