b97c9e3cca1a20a6f6d91fe25dd09cd9e8aaf29b
Cerek Hillen <crockeo@gmail.com>
2015-02-22 23:27:05 -0500

    Added engineconfig & font to the clibgame.hpp include.

--- src/clibgame.hpp
+++ src/clibgame.hpp
@@ -9,9 +9,11 @@
 
 #include "clibgame/res/animation.hpp"
 #include "clibgame/res/texsheet.hpp"
+#include "clibgame/engineconfig.hpp"
 #include "clibgame/res/texable.hpp"
 #include "clibgame/res/texture.hpp"
 #include "clibgame/res/shader.hpp"
+#include "clibgame/res/font.hpp"
 #include "clibgame/eventing.hpp"
 #include "clibgame/engine.hpp"
 #include "clibgame/delta.hpp"
