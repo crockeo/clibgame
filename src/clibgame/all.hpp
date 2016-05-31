0ed79baf94acf937c67b9761e360040cb60c0e90
Cerek Hillen <crockeo@gmail.com>
2016-05-31 12:42:29 -0400

    More progress.

---
+++ src/clibgame/all.hpp
@@ -0,0 +1,17 @@
+// Name     : clibgame/res/all.hpp
+// Author(s): Cerek Hillen
+// Init Date: 2016-05-31
+// Edit Date: 2016-05-31
+//
+// Description:
+//   Including all of the headers contained witin the resource library.
+
+#ifndef _CLIBGAME_RES_ALL_HPP_
+#define _CLIBGAME_RES_ALL_HPP_
+
+//////////////
+// Includes //
+
+// TODO: Finalize the rest of the library first.
+
+#endif
---
+++ src/clibgame/core/all.hpp
@@ -0,0 +1,18 @@
+// Name     : clibgame/core/all.hpp
+// Author(s): Cerek Hillen
+// Init Date: 2016-05-31
+// Edit Date: 2016-05-31
+//
+// Description:
+//   Includes all of the other headers from the core library.
+
+#ifndef _CLIBGAME_CORE_ALL_HPP_
+#define _CLIBGAME_CORE_ALL_HPP_
+
+//////////////
+// Includes //
+
+#include "debug.hpp"
+#include "uuid.hpp"
+
+#endif
