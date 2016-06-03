56fba7c0928abb5de443c3b981ca073fb5fc6b88
Cerek Hillen <crockeo@gmail.com>
2016-06-03 15:30:28 -0400

    'Started' on sounds.

---
+++ src/clibgame/res/sound.hpp
@@ -0,0 +1,20 @@
+// Name     : clibgame/res/sound.hpp
+// Author(s): Cerek Hillen
+// Init Date: 2016-06-03
+// Edit Date: 2016-06-03
+//
+// Description:
+//   Loading sounds (.ogg) files from the filesystem.
+
+#ifndef _CLIBGAME_RES_SOUND_HPP_
+#define _CLIBGAME_RES_SOUND_HPP_
+
+//////////
+// Code //
+
+namespace clibgame {
+    namespace res {
+    }
+}
+
+#endif
